12eced4b26eece5f8720fd4c994e58a16494f45d
m81923@gmail.com <JunHong Lin>
2021-03-15 16:49:40 +0800

    Update assignment: cookie

--- remote-assignments/Week-3/Assignments/app.js
+++ remote-assignments/Week-3/Assignments/app.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
 app.set('view engine', 'pug');
 
-// create root route
+// asignment-1
 app.get('/', (req, res) => {
   res.send('<h1>Hello, My Server!</h1>');
 });
 
+// asignment-2
 app.get('/data', (req, res) => {
   let num = Number(req.query.number);
   if (!req.query.number) {
@@ -26,26 +27,23 @@ app.get('/data', (req, res) => {
   }
 });
 
+// asignment-4
 app.get('/myName', (req, res) => {
-  let name = req.cookies.username;
-  // console.log(req.body.username);
-  if (name) {
-    res.send(`Hi ${name}`);
+  if (req.cookies.cookieName) { // check if cookieName in request
+    res.send(`Hi ${req.cookies.cookieName}`);
   } else {
-      res.render('hello');
-      // res.redirect('myName');
+    res.render('hello');
   }
 });
 
 app.get('/trackName', (req, res) => {
-  res.redirect('/myName');
+  if (req.query.username) {
+    console.log('catch req.body.username');
+    res.cookie( 'cookieName', req.query.username ); // store cookie as cookieName
+  }
+  return res.redirect('/myName');
 });
 
-app.post('/myName', (req, res) => {
-  console.log('post request to myName');
-  res.cookie('username', req.body.username);
-  res.redirect('/myName');
-});
 
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000.');
